6d9ce3d5e9d3d66e36764c31b2e37f33d9417d6d
YUS / PIS <49065681+hafisyusril@users.noreply.github.com>
2025-07-24 15:53:27 +0700

    feat: add login (#2)

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,19 @@
 import { ApiError } from "../../utils/api-error";
+import { JwtService } from "../jwt/jwt.service";
 import { PasswordService } from "../password/password.service";
 import { PrismaService } from "../prisma/prisma.service";
+import { LoginDTO } from "./dto/login.dto";
 import { RegisterDTO } from "./dto/register.dto";
 
 export class AuthService {
   private prisma: PrismaService;
   private passwordService: PasswordService;
+  private jwtService: JwtService;
 
   constructor() {
     this.prisma = new PrismaService();
     this.passwordService = new PasswordService();
+    this.jwtService = new JwtService();
   }
 
   register = async (body: RegisterDTO) => {
@@ -29,5 +33,26 @@ export class AuthService {
       data: { name: body.name, email: body.email, password: hashedPassword },
       omit: { password: true },
     });
+  };
+
+  login = async (body: LoginDTO) => {
+    const user = await this.prisma.user.findFirst({
+      where: { email: body.email },
+    });
+
+    if (!user) {
+      throw new ApiError(`Invalid credentiasl`, 400);
+    }
+    const isPasswordValid = await this.passwordService.comparePassword(
+      body.password,
+      user?.password
+    );
+    if (!isPasswordValid) {
+      throw new ApiError(`Invalid credentials`, 400);
+    }
+    const payload = {id: user.id}
+    const accessToken = this.jwtService.generateToken(payload, process.env.JWT_SECRET!, {expiresIn: '2h'})
+    const {password, ...userWithoutPassword} = user;
+    return {...userWithoutPassword, accessToken}
   };
 }
