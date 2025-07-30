4be597a637c53530afc80a7859fbfa4e66e12b4b
YUS / PIS <yusrilhafis@gmail.com>
2025-07-30 17:20:37 +0700

    feat: create blog

---
+++ src/modules/blog/dto/create-blog.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateBlogDTO {
+  @IsNotEmpty()
+  @IsString()
+  title!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  category!: string;
+}
